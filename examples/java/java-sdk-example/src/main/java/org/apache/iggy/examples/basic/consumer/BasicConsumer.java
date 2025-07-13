ef63c468600b00791a780ae5c7c736b9c69d7d8d
Steven Eck <77412505+StevenEck@users.noreply.github.com>
2025-07-13 16:41:13 -0700

    Basic Java Consumer Example copyright and iinstruction #1893

--- examples/java/java-sdk-example/src/main/java/org/apache/iggy/examples/basic/consumer/BasicConsumer.java
+++ examples/java/java-sdk-example/src/main/java/org/apache/iggy/examples/basic/consumer/BasicConsumer.java
@@ -1,3 +1,21 @@
+/* Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.iggy.examples.basic.consumer;
 
 import java.math.BigInteger;
@@ -19,7 +37,17 @@ import org.apache.iggy.topic.CompressionAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+/**
+ * BasicConsumer example for the Iggy Java SDK.
+ *
+ * PREREQUISITES:
+ * Before running this example, you need to start the Iggy server:
+ *
+ *    cargo run --bin iggy-server
+ *
+ * This will start a local Iggy server instance on the default port (8090)
+ * which this example connects to.
+ */
 
 public class BasicConsumer {
     private static final Logger logger = LoggerFactory.getLogger(BasicConsumer.class);
